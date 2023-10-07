445b3dc3f9ee4b345a128cf820494dab820becb7
Elenaudoel <137452488+Elenaudoel@users.noreply.github.com>
2023-10-07 10:02:37 -0300

    aula 8

---
+++ aula 8/script.js
@@ -0,0 +1,40 @@
+document.getElementById('titulo').innerHTML='bom dia'
+document.write(5+5)
+window.localStorage('teste123')
+
+
+
+// let resposta = window.prompt('digite um numero')
+// console.log(respota)
+// document.write(respota)
+
+
+
+// let resposta = window.prompt('digite um numero')
+// let num = resposta % 2
+
+
+if(num == 0){
+
+    document.write('e par')
+
+}
+else{
+    document.write('e impar')
+
+}
+
+// let num1  = parseInt(window.prompt('digite um numero'))
+// let num2= parseInt(window.prompt('digite um numero'))
+// document.write(num1+num2)
+
+
+
+let num = parseInt(window.prompt('digite um numero'))
+for (let t =5; t <11; num++){
+    document.write(num*t)
+}
+
+
+
+
